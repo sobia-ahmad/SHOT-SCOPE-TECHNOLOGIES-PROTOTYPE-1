401f4a95c5a1d5ee784b2d963798eda4f46afec9
Sobia Ahmad <63017839+sobia-ahmad@users.noreply.github.com>
2020-08-26 13:49:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// STICKY NAVBAR IMPLEMENTATION
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
